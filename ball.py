5c6fd94e9701b1d2273cf071ccc7fcc09b1bec9e
Andrew Pan <andrew.pan@students.olin.edu>
2016-04-07 14:54:45 -0400

    basic ball class

--- ball.py
+++ ball.py
@@ -1,1 +1,22 @@
 #ball class created here, holds ball positions, number, etc.
+import math
+
+class ball:
+	def __init__(self, xpos, ypos, number, pocketed = False, pocketNum = 0, xv = 0, yv = 0, ballRadius = 5):
+		self.pos = [xpos, ypos]
+		self.num = number
+		self.pstate = pocketed
+		self.pocket = pocketNum
+		self.vel = [xv, yv]
+		self.radius = ballRadius
+	def checkCollision(self, otherBall):
+		#returns true if colliding with the other ball, else if no collision
+		return math.sqrt((self.pos[0] - otherBall.pos[0])**2 + (self.pos[1] - otherBall.pos[1])**2) <= (radius * 2)
+	def checkPocketed(self, pocketList, pocketRadius):
+		for p in pocketList:
+			if math.sqrt((self.pos[0] - otherBall.pos[0])**2 + (p.pos[1] - p.pos[1])**2) < (radius + pocketRadius):
+				self.pocketed = True
+				self.pocketNum = p.num
+				return True
+		return False
+		
