86949e9cba4f7f9049c03823fe8d36252303786c
Jeremy Garcia <jeremiah.garcia@students.olin.edu>
2016-04-11 00:48:44 -0400

    display pool table

--- draw-table.py
+++ draw-table.py
@@ -1,1 +1,17 @@
 #basic display graphics, draw table with pockets and balls given a list of ball objects
+import pygame
+import math
+
+screen = pygame.display.set_mode((1579,873))
+
+table = pygame.image.load('pool_table.png')
+
+holes = ((83,78),(790,57),(1499,80),(83,791),(790,810),(1499,790))
+
+running = True
+while running:
+	screen.blit(table,(0,0))
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+	pygame.display.update()
