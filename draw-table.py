fce2d31e192c7839366294c3853b5840eebb832f
Jeremy Garcia <jeremiah.garcia@students.olin.edu>
2016-04-11 15:08:30 -0400

    added Table class

--- draw-table.py
+++ draw-table.py
@@ -2,16 +2,21 @@
 import pygame
 import math
 
+class Table(object):
+	def __init__(self, pockets ,top = 84,bottom = 789,left = 84,right =1494, ball_list):
+		self.pockets = ((83,78),(790,57),(1499,80),(83,791),(790,810),(1499,790))
+		self.walls = [top, bottom, left, right]
+		self.ball_list = ball_list
+
 screen = pygame.display.set_mode((1579,873))
 
 table = pygame.image.load('pool_table.png')
 
-holes = ((83,78),(790,57),(1499,80),(83,791),(790,810),(1499,790))
-
-running = True
-while running:
-	screen.blit(table,(0,0))
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-	pygame.display.update()
+def draw_table():
+	running = True
+	while running:
+		screen.blit(table,(0,0))
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+		pygame.display.update()
