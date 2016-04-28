aaa331adf288f31ee2e72ecbbc7072d4c3fdb761
John Moreland <jmoreland@outlook.com>
2016-04-28 14:38:10 -0400

    Bug fixes and cleaned up code. Added docstring and appropriate creditsd

--- calibration.py
+++ calibration.py
@@ -1,9 +1,13 @@
 '''
 Allows user to calibrate to pool table
+
+Instructions: Set an input image, click on four corners in order (TL, TR, BR, BL)
+
+Credits: Code is adapted from Adrian Rosebrock's "4 point openCV Example"
+            on pyimagesearch.com
 '''
 import numpy as np
 import cv2
-
 
 def order_points(pts):
     # initialzie a list of coordinates that will be ordered
@@ -11,20 +15,20 @@ def order_points(pts):
     # the second entry is the top-right, the third is the
     # bottom-right, and the fourth is the bottom-left
     rect = np.zeros((4, 2), dtype = "float32")
- 
+
     # the top-left point will have the smallest sum, whereas
     # the bottom-right point will have the largest sum
     s = pts.sum(axis = 1)
     rect[0] = pts[np.argmin(s)]
     rect[2] = pts[np.argmax(s)]
- 
+
     # now, compute the difference between the points, the
     # top-right point will have the smallest difference,
     # whereas the bottom-left will have the largest difference
     diff = np.diff(pts, axis = 1)
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
- 
+
     # return the ordered coordinates
     return rect
 
@@ -33,21 +37,21 @@ def four_point_transform(image, pts):
     # individually
     rect = order_points(pts)
     (tl, tr, br, bl) = rect
- 
+
     # compute the width of the new image, which will be the
     # maximum distance between bottom-right and bottom-left
     # x-coordiates or the top-right and top-left x-coordinates
     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
     maxWidth = max(int(widthA), int(widthB))
- 
+
     # compute the height of the new image, which will be the
     # maximum distance between the top-right and bottom-right
     # y-coordinates or the top-left and bottom-left y-coordinates
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
     maxHeight = max(int(heightA), int(heightB))
- 
+
     # now that we have the dimensions of the new image, construct
     # the set of destination points to obtain a "birds eye view",
     # (i.e. top-down view) of the image, again specifying points
@@ -58,45 +62,44 @@ def four_point_transform(image, pts):
         [maxWidth - 1, 0],
         [maxWidth - 1, maxHeight - 1],
         [0, maxHeight - 1]], dtype = "float32")
- 
+
     # compute the perspective transform matrix and then apply it
     M = cv2.getPerspectiveTransform(rect, dst)
     warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
- 
+
     # return the warped image
     return warped
 
 ref_pts = []
-
 def store_points(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
         global ref_pts
-        coords =[(x, y)]
+        coords = (x, y)
         ref_pts.append(coords)
 
+# set input image
 img = cv2.imread('pool_pic.JPG')
-# cv2.namedWindow('original')
-# cv2.setMouseCallback('original', store_points)
 
-# while True:
-#     cv2.imshow('original',img)
-#     if len(ref_pts) > 3:
-#         pts = np.array(ref_pts)
-#         warped = four_point_transform(img, pts)
-#         cv2.imshow('warped', warped)
-#         cv2.waitKey(0)
-#         print 'deuces'
-#         break
+# initialize the window and set interaction function
+cv2.namedWindow('original')
+cv2.setMouseCallback('original', store_points)
 
-#     #press esc to exit
-#     key = cv2.waitKey(10)
-#     if key == 27:
-#         break
-# cv2.destroyAllWindows()
+while True:
+    # display the input image
+    cv2.imshow('original',img)
 
-pts = np.array([(150,40), (1050,55), (1250,450), (8,470)])
-warped = four_point_transform(img, pts)
-cv2.imshow('original', img)
-cv2.imshow('warped', warped)
-cv2.imwrite('warped.jpg', warped)
-cv2.waitKey(0)
+    # wait until there have been 4 clicks
+    if len(ref_pts) > 3:
+        pts = np.array(ref_pts)
+        warped = four_point_transform(img, pts)
+
+        #display the transformed image
+        cv2.imshow('warped', warped)
+        cv2.waitKey(0)
+        break
+
+    #press esc to exit
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
+cv2.destroyAllWindows()
